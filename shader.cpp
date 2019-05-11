630ffbdbc2967d0916d1c590737b0c5bb6df3b23
Nick <scifi6546@protonmail.com>
2019-05-11 06:38:33 -0800

    added more to gameShader namespace

--- shader.cpp
+++ shader.cpp
@@ -3,20 +3,27 @@
 #include "error.h"
 #include <glm/glm.hpp>
 #include <glm/ext.hpp>
-static const unsigned int NUM_SHADERS=2;
-GLuint program;
-GLuint shaders[NUM_SHADERS];
-glm::vec3 camera_pos;
-GLuint camera_uni_pos;
-GLuint translate_uni_pos;
-GLuint look_uni_pos;
-GLuint image_uni_pos;
-GLuint ambient_c_pos;
-GLuint ambient_i_pos;
 
-GLuint sun_pos_pos;
-GLuint sun_intensity_pos;
-GLuint sun_c_pos;
+namespace gameShader{
+    static const unsigned int NUM_SHADERS=2;
+    GLuint program;
+    GLuint shaders[NUM_SHADERS];
+    glm::vec3 camera_pos;
+    GLuint camera_uni_pos;
+    GLuint translate_uni_pos;
+    GLuint look_uni_pos;
+    GLuint image_uni_pos;
+    GLuint ambient_c_pos;
+    GLuint ambient_i_pos;
+    
+    GLuint sun_pos_pos;
+    GLuint sun_intensity_pos;
+    GLuint sun_c_pos;
+}
+namespace bufferShader{
+
+}
+
 int initGameShader();
 int initBufferShader();
 int shaderInit(){
@@ -24,47 +31,47 @@ int shaderInit(){
 }
 int initGameShader(){
     std::string filename = "./shaders/shader";
-    program=glCreateProgram();
+    gameShader::program=glCreateProgram();
     glError = glGetError();
     std::string file = loadFile(filename + ".vs");
-    shaders[0] = createShader(file,GL_VERTEX_SHADER);
+    gameShader::shaders[0] = createShader(file,GL_VERTEX_SHADER);
     file = loadFile(filename + ".fs");
-    shaders[1] = createShader(file,GL_FRAGMENT_SHADER);
-    for(unsigned int i=0;i<NUM_SHADERS;i++){
-        glAttachShader(program,shaders[i]);
+    gameShader::shaders[1] = createShader(file,GL_FRAGMENT_SHADER);
+    for(unsigned int i=0;i<gameShader::NUM_SHADERS;i++){
+        glAttachShader(gameShader::program,gameShader::shaders[i]);
     }
-    glBindAttribLocation(program,0,"position");
-    glBindAttribLocation(program,1,"texcoord");
-    glLinkProgram(program);
+    glBindAttribLocation(gameShader::program,0,"position");
+    glBindAttribLocation(gameShader::program,1,"texcoord");
+    glLinkProgram(gameShader::program);
     int status;
-    status = checkShaderError(program,GL_LINK_STATUS,true,"Error: Program failed to link");
+    status = checkShaderError(gameShader::program,GL_LINK_STATUS,true,"Error: Program failed to link");
     if(status!=0)
         return status;
 
-    glValidateProgram(program);
-    status = checkShaderError(program,GL_VALIDATE_STATUS,true,"program failed to validate");
+    glValidateProgram(gameShader::program);
+    status = checkShaderError(gameShader::program,GL_VALIDATE_STATUS,true,"program failed to validate");
     if(status!=0)
         return status;
     bind();
     glError=glGetError();  
     //setting uniforms
-    camera_uni_pos =  glGetUniformLocation(program,"camera");
-    translate_uni_pos =  glGetUniformLocation(program,"translate");
-    look_uni_pos =  glGetUniformLocation(program,"look");
-    image_uni_pos = glGetUniformLocation(program,"diffuse");
-    ambient_c_pos = glGetUniformLocation(program,"ambient_color");
-    ambient_i_pos = glGetUniformLocation(program,"ambient_intensity");
-
-    sun_intensity_pos = glGetUniformLocation(program,"sun_intensity");
-    sun_pos_pos = glGetUniformLocation(program,"sun_pos");
-    sun_c_pos = glGetUniformLocation(program,"sun_color");
+    gameShader::camera_uni_pos =  glGetUniformLocation(gameShader::program,"camera");
+    gameShader::translate_uni_pos =  glGetUniformLocation(gameShader::program,"translate");
+    gameShader::look_uni_pos =  glGetUniformLocation(gameShader::program,"look");
+    gameShader::image_uni_pos = glGetUniformLocation(gameShader::program,"diffuse");
+    gameShader::ambient_c_pos = glGetUniformLocation(gameShader::program,"ambient_color");
+    gameShader::ambient_i_pos = glGetUniformLocation(gameShader::program,"ambient_intensity");
+    gameShader::sun_intensity_pos = glGetUniformLocation(gameShader::program,"sun_intensity");
+    gameShader::sun_pos_pos = glGetUniformLocation(gameShader::program,"sun_pos");
+    gameShader::sun_c_pos = glGetUniformLocation(gameShader::program,"sun_color");
 
     glError=glGetError();
-    printf("camera_uni_pos = %i\n",camera_uni_pos);
-    printf("translate_uni_pos = %i\n",translate_uni_pos);
-    printf("look_uni_pos = %i\n",look_uni_pos);
-    printf("program:%i\n",shaders[0]);
+    printf("camera_uni_pos = %i\n",gameShader::camera_uni_pos);
+    printf("translate_uni_pos = %i\n",gameShader::translate_uni_pos);
+    printf("look_uni_pos = %i\n",gameShader::look_uni_pos);
+    printf("program:%i\n",gameShader::shaders[0]);
 }
+
 int checkShaderError(GLuint shader, GLuint flag, bool isProgram, std::string errorMessage){
         GLint success = 0;
     GLchar error[1024] = { 0 };
@@ -102,28 +109,28 @@ GLuint createShader(std::string &text,GLenum shaderType){
 }
 void sendCamera(glm::mat4 cam){
     //finding uniform in shaderinit()
-    glUniformMatrix4fv(camera_uni_pos,1,GL_FALSE,glm::value_ptr(cam));
+    glUniformMatrix4fv(gameShader::camera_uni_pos,1,GL_FALSE,glm::value_ptr(cam));
 }
 void sendTranslate(glm::mat4 trans){
     //finding uniform in shaderinit()
-    glUniformMatrix4fv(translate_uni_pos,1,GL_FALSE,glm::value_ptr(trans));
+    glUniformMatrix4fv(gameShader::translate_uni_pos,1,GL_FALSE,glm::value_ptr(trans));
 }
 
 void sendLook(glm::mat4 look){
     //finding uniform in shaderinit()
-    glUniformMatrix4fv(look_uni_pos,1,GL_FALSE,glm::value_ptr(look));
+    glUniformMatrix4fv(gameShader::look_uni_pos,1,GL_FALSE,glm::value_ptr(look));
 }
 void sendAmbient(glm::vec3 color,GLfloat intensity,glm::vec3 sun_pos,GLfloat sun_intensity,glm::vec3 sun_color){
-    glUniform3f(ambient_c_pos,color.x,color.y,color.z);
-    glUniform1f(ambient_i_pos,intensity);
+    glUniform3f(gameShader::ambient_c_pos,color.x,color.y,color.z);
+    glUniform1f(gameShader::ambient_i_pos,intensity);
 
-    glUniform1f(sun_intensity_pos,sun_intensity);
-    glUniform3f(sun_pos_pos,sun_pos.x,sun_pos.y,sun_pos.z);
-    glUniform3f(sun_c_pos,sun_color.x,sun_color.y,sun_color.z);
+    glUniform1f(gameShader::sun_intensity_pos,sun_intensity);
+    glUniform3f(gameShader::sun_pos_pos,sun_pos.x,sun_pos.y,sun_pos.z);
+    glUniform3f(gameShader::sun_c_pos,sun_color.x,sun_color.y,sun_color.z);
 }
 void setTexture(int unit){
     //printf("unit = %i\n",unit);
-    glUniform1i(image_uni_pos,unit);
+    glUniform1i(gameShader::image_uni_pos,unit);
     glError = glGetError();
     if(glError!=0){
         printf("error %i\n",glError);
@@ -131,9 +138,9 @@ void setTexture(int unit){
 }
 
 void bind(){
-    for(unsigned int i=0;i<NUM_SHADERS;i++){
-        glDetachShader(program,shaders[i]);
-        glDeleteShader(shaders[i]);
+    for(unsigned int i=0;i<gameShader::NUM_SHADERS;i++){
+        glDetachShader(gameShader::program,gameShader::shaders[i]);
+        glDeleteShader(gameShader::shaders[i]);
     }
-     glUseProgram(program);
+     glUseProgram(gameShader::program);
 }
